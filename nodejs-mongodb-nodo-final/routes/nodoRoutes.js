9be74a0fd4eb4410a8ff5f93854af71f985aa489
Sara Cortes <84357190+SaraCmque@users.noreply.github.com>
2023-06-04 17:36:00 -0500

    Update nodoRoutes.js

--- nodejs-mongodb-nodo-final/routes/nodoRoutes.js
+++ nodejs-mongodb-nodo-final/routes/nodoRoutes.js
@@ -17,7 +17,7 @@ app.get("/Teachers", async (request, response) => {
 
 //insertar: URI -> nodo/TeacherInsert
 
-app.post("/insert", async (request, response) => {
+app.post("/TeacherInsert", async (request, response) => {
   const teacher = new nodoModel.Teacher(request.body);
 
   try {
@@ -28,8 +28,8 @@ app.post("/insert", async (request, response) => {
   }
 });
 
-// URI: /nodo/actualizar/{id}
-app.patch("/update/:id", async (request, response) => {
+// URI: /nodo/TeacherUpdate/id
+app.patch("/TeacherUpdate/:id", async (request, response) => {
   try {
     const teacher = new nodoModel.Teacher(request.body);
     await nodoModel.Teacher.findByIdAndUpdate(request.params.id, request.body);
@@ -40,13 +40,60 @@ app.patch("/update/:id", async (request, response) => {
   }
 });
 
-app.delete("/eliminar/:id", async (request, response) => {
+app.delete("/TeacherDelete/:id", async (request, response) => {
   try {
     const teacher = await nodoModel.Teacher.findByIdAndDelete(
       request.params.id
     );
 
     if (!teacher) response.status(404).send("No teacher found");
+    response.status(200).send();
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+//URIS para Students
+
+app.get("/Students", async (request, response) => {
+  const students = await nodoModel.Student.find({});
+
+  try {
+    response.send(students);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.post("/StudentInsert", async (request, response) => {
+  const student = new nodoModel.Student(request.body);
+
+  try {
+    await student.save();
+    response.send(student);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.patch("/StudentUpdate/:id", async (request, response) => {
+  try {
+    const student = new nodoModel.Student(request.body);
+    await nodoModel.Student.findByIdAndUpdate(request.params.id, request.body);
+    await nodoModel.Student.save();
+    response.send(student);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.delete("/StudentDelete/:id", async (request, response) => {
+  try {
+    const student = await nodoModel.Student.findByIdAndDelete(
+      request.params.id
+    );
+
+    if (!student) response.status(404).send("No student found");
     response.status(200).send();
   } catch (error) {
     response.status(500).send(error);
