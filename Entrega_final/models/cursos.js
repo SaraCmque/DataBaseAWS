42e6ee00337b592066f49cc855ce80ec09d70455
Sara Cortes <84357190+SaraCmque@users.noreply.github.com>
2023-06-06 17:12:32 -0500

    Add files via upload

---
+++ Entrega_final/models/cursos.js
@@ -0,0 +1,16 @@
+const conexion = require("../conexion");
+
+module.exports = {
+  obtener() {
+    return new Promise((resolve, reject) => {
+      conexion.query(
+        `select distinct c.id_curso, c.nombre, i.categoria, i.fecha_inicio, i.fecha_fin 
+        from nodo_entrega4.curso c inner join nodo_entrega4.info_curso i on c.id_curso = i.id_curso`,
+        (err, resultados) => {
+          if (err) reject(err);
+          else resolve(resultados);
+        }
+      );
+    });
+  },
+};
