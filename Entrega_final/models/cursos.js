3c80a497bddc52f67b71e8b3a1bf6497b2f250a2
Sara Cortes <84357190+SaraCmque@users.noreply.github.com>
2023-06-07 11:42:46 -0500

    Update cursos.js

--- Entrega_final/models/cursos.js
+++ Entrega_final/models/cursos.js
@@ -18,7 +18,7 @@ module.exports = {
       conexion.query(
         `select distinct c.id_curso, ic.id_info, c.precio, c.url, pc.año, pc.semestre,
         p.nombre_completo from nodo_entrega3.curso c inner join nodo_entrega3.info_curso
-        ic on c.id_curso = ic.id_curso inner join nodo_entrega4.profesor_curso pc on c.id_curso = pc.id_curso
+        ic on c.id_curso = ic.id_curso inner join nodo_entrega3.profesor_curso pc on c.id_curso = pc.id_curso
         inner join nodo_entrega3.profesor p on pc.doc_id = p.doc_id where ic.id_info = ?`,
         [id_info],
         (err, resultados) => {
