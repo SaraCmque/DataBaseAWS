3e9e4f14ff6f1431e2a9df0202e006f93c1db42a
Sara Cortes <84357190+SaraCmque@users.noreply.github.com>
2023-06-07 11:48:58 -0500

    Update cursos.js

--- Entrega_final/models/cursos.js
+++ Entrega_final/models/cursos.js
@@ -4,8 +4,8 @@ module.exports = {
   obtener() {
     return new Promise((resolve, reject) => {
       conexion.query(
-        `select distinct c.id_curso, c.nombre, i.categoria, i.fecha_inicio, i.fecha_fin 
-        from nodo_entrega3.curso c inner join nodo_entrega3.info_curso i on c.id_curso = i.id_curso`,
+        `select distinct i.id_info, c.id_curso, c.nombre, i.categoria, i.fecha_inicio, i.fecha_fin 
+from nodo_entrega4.curso c inner join nodo_entrega4.info_curso i on c.id_curso = i.id_curso;`,
         (err, resultados) => {
           if (err) reject(err);
           else resolve(resultados);
