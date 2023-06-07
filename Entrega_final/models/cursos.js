bd5940b166f54273c812de6edc37de305fe4e029
Sara Cortes <84357190+SaraCmque@users.noreply.github.com>
2023-06-07 10:49:48 -0500

    Update cursos.js

--- Entrega_final/models/cursos.js
+++ Entrega_final/models/cursos.js
@@ -13,4 +13,18 @@ module.exports = {
       );
     });
   },
+  obtenerDetalles(id_curso) {
+    return new Promise((resolve, reject) => {
+      conexion.query(
+        `select distinct c.precio, c.url, pc.año, pc.semestre, p.nombre_completo from nodo_entrega4.curso c inner join nodo_entrega4.info_curso
+        ic on c.id_curso = ic.id_curso inner join nodo_entrega4.profesor_curso pc on c.id_curso = pc.id_curso
+        inner join nodo_entrega4.profesor p on pc.doc_id = p.doc_id where c.id_curso = ?`,
+        [id_curso],
+        (err, resultados) => {
+          if (err) reject(err);
+          else resolve(resultados);
+        }
+      );
+    });
+  },
 };
