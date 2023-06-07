e2c53b175a9b8aa12938ec08d86cceab5f8cbefb
Sara Cortes <84357190+SaraCmque@users.noreply.github.com>
2023-06-07 10:48:40 -0500

    Update cursoRoutes.js

--- Entrega_final/routes/cursoRoutes.js
+++ Entrega_final/routes/cursoRoutes.js
@@ -16,4 +16,17 @@ router.get("/", function (req, res, next) {
     });
 });
 
+router.get("/verInfoCurso/:id_curso", function (req, res, next) {
+  cursoModel
+    .obtenerDetalles(req.params.id_curso)
+    .then((curso) => {
+      res.render("curso/verInfoCurso", {
+        curso: curso,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).send("Error obteniendo Detalles");
+    });
+});
+
 module.exports = router;
