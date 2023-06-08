093aac99ffb84ffcb8d7f4a32435792db0e462ff
Sara Cortes <84357190+SaraCmque@users.noreply.github.com>
2023-06-07 22:20:11 -0500

    Update cursos.js

--- Entrega_final/models/cursos.js
+++ Entrega_final/models/cursos.js
@@ -17,9 +17,9 @@ from nodo_entrega3.curso c inner join nodo_entrega3.info_curso i on c.id_curso =
     return new Promise((resolve, reject) => {
       conexion.query(
         `select distinct c.id_curso, ic.id_info, c.precio, c.url, pc.año, pc.semestre,
-        p.nombre_completo from curso c inner join info_curso
-        ic on c.id_curso = ic.id_curso inner join profesor_curso pc on c.id_curso = pc.id_curso
-        inner join profesor p on pc.doc_id = p.doc_id where ic.id_info = ?`,
+        p.nombre_completo from nodo_entrega3.curso c inner join nodo_entrega3.info_curso
+        ic on c.id_curso = ic.id_curso inner join nodo_entrega3.profesor_curso pc on c.id_curso = pc.id_curso
+        inner join nodo_entrega3.profesor p on pc.doc_id = p.doc_id where ic.id_info = ?`,
         [id_info],
         (err, resultados) => {
           if (err) reject(err);
