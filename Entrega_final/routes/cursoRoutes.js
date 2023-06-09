639e3311aacde2fbfc9b71724abf20fdea448741
Sara Cortes <84357190+SaraCmque@users.noreply.github.com>
2023-06-09 15:31:19 -0500

    Update cursoRoutes.js

--- Entrega_final/routes/cursoRoutes.js
+++ Entrega_final/routes/cursoRoutes.js
@@ -34,10 +34,10 @@ router.get("/verinfocurso/:id_info", function (req, res, next) {
 router.get("/verinfocurso/:id_info", function (req, res, next) {
   cursoModel
     .obtenerEstudiantes(req.params.id_info)
-    .then((estudiantes) => {
-      if (estudiantes) {
+    .then((estudiante) => {
+      if (estudiante) {
         res.render("curso/verinfocurso", {
-          estudiantes: estudiantes,
+          estudiante: estudiante,
         });
       }
     })
