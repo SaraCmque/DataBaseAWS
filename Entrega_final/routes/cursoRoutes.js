339a5d1d9169a59a93e1ef0f68d1b75a750a2ba3
Sara Cortes <84357190+SaraCmque@users.noreply.github.com>
2023-06-09 15:44:25 -0500

    Update cursoRoutes.js

--- Entrega_final/routes/cursoRoutes.js
+++ Entrega_final/routes/cursoRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const cursoModel = require("../models/cursos");
+const estudianteModel = require("../models/cursos");
 
 router.get("/", function (req, res, next) {
   cursoModel
@@ -15,7 +16,7 @@ router.get("/", function (req, res, next) {
       return res.status(500).send("Error obteniendo Cursos");
     });
 });
-
+/*
 router.get("/verinfocurso/:id_info", function (req, res, next) {
   cursoModel
     .obtenerDetalles(req.params.id_info)
@@ -45,5 +46,24 @@ router.get("/verinfocurso/:id_info", function (req, res, next) {
       return res.status(500).send("Error obteniendo Estudiantes");
     });
 });
+*/
+router.get("/verInfoCurso/:id_info", function (req, res, next) {
+  const id_info = req.params.id_info;
+  Promise.all([
+    cursoModel.obtenerDetalles(id_info),
+    estudianteModel.obtenerEstudiantes(id_info), // Replace with the actual function to fetch data for the second table
+  ])
+    .then(([curso, estudiante]) => {
+      res.render("curso/verInfoCurso", {
+        curso: curso,
+        estudiante: estudiante,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).send("Error obteniendo Detalles");
+    });
+});
+
+
 
 module.exports = router;
