7b69f7f7ef2fb9c6863743b09af376843d38bb95
Sara Cortes <84357190+SaraCmque@users.noreply.github.com>
2023-06-09 23:10:15 -0500

    Update profesor.js

--- Entrega_final/models/profesor.js
+++ Entrega_final/models/profesor.js
@@ -4,7 +4,7 @@ module.exports = {
   insertar(doc_id, nombre_completo, email, telefono, contrasena) {
     return new Promise((resolve, reject) => {
       conexion.query(
-        `insert into profesor(doc_id, nombre_completo, email, telefono, contrasena) values (?, ?, ?,?,?,?)`,
+        `insert into profesor(doc_id, nombre_completo, email, telefono, contrasena) values (?, ?, ?,?,?)`,
         [doc_id, nombre_completo, email, telefono, contrasena],
         (err, resultados) => {
           if (err) reject(err);
